22c00a2d939d779b6a8494b3485cee5afc73cb37
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-06-26 23:24:18 +0300

    Bump version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkdocs-plugin-rzk',
-    version='0.1.1',
+    version='0.1.2',
     description='A MkDocs plugin for Literate Rzk',
     long_description='This plugin automates the generation of SVG renderings for code snippets in Literate Rzk Markdown files rendered using MkDocs.',
     keywords='mkdocs',
